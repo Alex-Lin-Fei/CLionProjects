3ab9633cdba0cb3433ace4635aff365eb6aaadfa
Alex-Lin-Fei <2055017970@qq.com>
2021-05-04 19:24:43 +0800

    add expert c programming

---
+++ ExpertCProgramming/chapter1/cmake-build-debug/CMakeFiles/chapter1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter1.dir/main.c.o"
+  "chapter1"
+  "chapter1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
