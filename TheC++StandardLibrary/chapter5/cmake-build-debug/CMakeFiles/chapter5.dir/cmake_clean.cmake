b66c2972bf578d19fe100e8d0ae57ceb6ee9cb92
Alex-Lin-Fei <2055017970@qq.com>
2021-05-05 17:44:25 +0800

    some tools in c++

---
+++ TheC++StandardLibrary/chapter5/cmake-build-debug/CMakeFiles/chapter5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter5.dir/main.cpp.o"
+  "chapter5"
+  "chapter5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
