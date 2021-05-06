1afe52cc7dfa4d2cd8b0a97fa81469dfa495ad51
Alex-Lin-Fei <2055017970@qq.com>
2021-05-06 10:30:46 +0800

    add some code

--- ExpertCProgramming/chapter2/main.c
+++ ExpertCProgramming/chapter2/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+char s[4];
+char* foo() {
+//    s[0]='a';
+    static char s[] = "abc";
+
+    return s;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int N = 10;
+    int * q = &N;
+    int p = N * sizeof * q;
+    int apple = sizeof(int) * p;
+
+    printf("p : %d; apple: %d; int: %lu\n", p, apple, sizeof(int));
+    int x = (1, N);
+    printf("x: %d\n", x);
+    printf("%s", foo());
+
     return 0;
 }
