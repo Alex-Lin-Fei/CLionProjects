f0eb502c3f1cc5de6e4f0d3f67c998a00bcda972
Alex-Lin-Fei <2055017970@qq.com>
2021-05-09 17:42:45 +0800

    add code

--- TheC++StandardLibrary/chapter6/main.cpp
+++ TheC++StandardLibrary/chapter6/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <array>
+#include <list>
+#include <unordered_set>
+using namespace std;
+
+void testArray() {
+    array<int, 5> nums{} ;
+    for (auto& i: nums)
+        cout << i << ' ';
+    cout << endl;
+    list<int> l{1,2,3};
+    cout << l.front() << ' ' << l.back();
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    testArray();
+
+    unordered_multiset<string> cities{
+            "Braunschweig", "Hanover", "Frankfurt", "New York", "Chicago", "Toronto", "Paris", "Frankfurt"
+    };
+
+    for (const auto &elem: cities) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    cities.insert({"London", "Munich", "Hanover", "Braunschweig"});
+
+    for (const auto &elem: cities) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
     return 0;
 }
