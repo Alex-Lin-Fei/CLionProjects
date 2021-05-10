06873f05b579d84b46e3ba52d7939de62ccbcc40
Alex-Lin-Fei <2055017970@qq.com>
2021-05-10 17:22:57 +0800

    add something

--- ExpertCProgramming/chapter4/main.cpp
+++ ExpertCProgramming/chapter4/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x = 12;
+    float y = x;
+    printf("x: %s %d\n", typeid(x).name(), x);
+    printf("y: %s %f\n", typeid(y).name(), y);
+
+    y = 12.0;
+    x = y;
+    printf("x: %s %d\n", typeid(x).name(), x);
+    printf("y: %s %f", typeid(y).name(), y);
     return 0;
 }
+
