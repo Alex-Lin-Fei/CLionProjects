b27a8181ea02bf8163fd1190cd4da2102f036044
Alex-Lin-Fei <2055017970@qq.com>
2021-05-10 17:51:19 +0800

    add note

--- ExpertCProgramming/chapter4/main.cpp
+++ ExpertCProgramming/chapter4/main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 
 int main() {
-    int x = 12;
-    float y = x;
-    printf("x: %s %d\n", typeid(x).name(), x);
-    printf("y: %s %f\n", typeid(y).name(), y);
 
-    y = 12.0;
-    x = y;
-    printf("x: %s %d\n", typeid(x).name(), x);
-    printf("y: %s %f", typeid(y).name(), y);
+
     return 0;
 }
 
