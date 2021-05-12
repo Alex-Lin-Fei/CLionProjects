74f6c278ad3f59bd36b9c3ec7d6875c29d94fc1c
Alex-Lin-Fei <2055017970@qq.com>
2021-05-12 21:55:35 +0800

    add something

--- ExpertCProgramming/chapter10/main.c
+++ ExpertCProgramming/chapter10/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <setjmp.h>
+
+int (*paf())[20] {
+    int (*pear)[20];
+    pear =calloc(20, sizeof(int));
+    if (!pear)
+        longjmp();
+    return pear;
+}
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    int x[n];
-    printf("Hello, World!\n");
+    int (*res)[20];
+    res = paf();
+    (*res)[1] = 12;
+    printf("%d, %d\n", (*res)[0], (*res)[1]);
+
     return 0;
 }
