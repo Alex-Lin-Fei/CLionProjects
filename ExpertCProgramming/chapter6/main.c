422a686f190408fe02c9db87a2edd100355016b9
Alex-Lin-Fei <2055017970@qq.com>
2021-05-13 11:31:53 +0800

    memory

--- ExpertCProgramming/chapter6/main.c
+++ ExpertCProgramming/chapter6/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <malloc.h>
+char pear[40];
+static double peach;
+
+char * s = "abcde";
+
+int mango = 13;
+int x = 1;
+static long melon = 2001;
 
 int main() {
-    printf("Hello, World!\n");
+    char z[] = "yue";
+    int i = 3, j , *ip;
+    ip = malloc(sizeof(i));
+    pear[5] = i;
+    peach = 2.0 * mango;
+
     return 0;
 }
