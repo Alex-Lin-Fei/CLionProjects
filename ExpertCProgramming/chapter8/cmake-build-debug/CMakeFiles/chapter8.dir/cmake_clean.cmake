43dbfb7d9e399b063f17ca1f55b8ed13b0c18433
Alex-Lin-Fei <2055017970@qq.com>
2021-05-16 14:36:51 +0800

    prototype

---
+++ ExpertCProgramming/chapter8/cmake-build-debug/CMakeFiles/chapter8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter8.dir/main.c.o"
+  "chapter8"
+  "chapter8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
