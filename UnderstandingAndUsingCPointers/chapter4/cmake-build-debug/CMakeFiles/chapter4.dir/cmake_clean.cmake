98a680115761950ee27103d1d3aa837fbf7fff02
Alex-Lin-Fei <2055017970@qq.com>
2021-05-21 16:06:27 +0800

    point and array

---
+++ UnderstandingAndUsingCPointers/chapter4/cmake-build-debug/CMakeFiles/chapter4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter4.dir/main.c.o"
+  "chapter4"
+  "chapter4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
