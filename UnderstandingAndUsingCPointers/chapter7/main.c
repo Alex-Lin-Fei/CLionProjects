59fa8bc393027ef2ca4c8c385856bf005f6b8705
Alex-Lin-Fei <2055017970@qq.com>
2021-05-22 12:04:51 +0800

    Security issues and pointer misuse

--- UnderstandingAndUsingCPointers/chapter7/main.c
+++ UnderstandingAndUsingCPointers/chapter7/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+//#define PINT int *
 
+typedef  int* PINT;
 int main() {
-    printf("Hello, World!\n");
+    PINT ptr1, ptr2;
+    ptr1 = (PINT) malloc(2);
+    printf("%d %d\n", sizeof ptr1, sizeof ptr2);
+    free(ptr1);
+//    ptr1 = NULL;
+    free(ptr1);
+
     return 0;
 }
+
