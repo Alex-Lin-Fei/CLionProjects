8aca0814cb79a0888f2e6ff4a237822c1ce95256
Alex-Lin-Fei <2055017970@qq.com>
2021-05-23 13:23:27 +0800

    pointer and struct

---
+++ UnderstandingAndUsingCPointers/chapter6/cmake-build-debug/CMakeFiles/chapter6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter6.dir/main.c.o"
+  "chapter6"
+  "chapter6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
