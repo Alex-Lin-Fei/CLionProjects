f03ec3f8bff1aa099b0c008047271fa1a3f34075
Alex-Lin-Fei <2055017970@qq.com>
2021-06-02 17:12:55 +0800

    add who

--- UULP/chapter1/main.c
+++ UULP/chapter1/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <errno.h>
 
 #define PAGELINES 24
 #define LINELEN 512
-
 void do_more(FILE *);
-int see_more();
+int see_more(FILE*);
+
 
 int main(int argc, char * argv[]) {
     FILE * fp;
@@ -28,10 +31,14 @@ int main(int argc, char * argv[]) {
 void do_more(FILE * fp) {
     char line[LINELEN];
     int num_of_lines=0;
-    int see_more(), reply;
+    int see_more(FILE *), reply;
+    FILE * fp_tty;
+    fp_tty = fopen("/dev/tty", "r");
+    if (fp_tty == NULL)
+        exit(1);
     while (fgets(line, LINELEN, fp)) {
         if (num_of_lines == PAGELINES) {
-            reply = see_more();
+            reply = see_more(fp_tty);
             if (reply == 0)
                 break;
             num_of_lines -= reply;
@@ -42,10 +49,10 @@ void do_more(FILE * fp) {
     }
 }
 
-int see_more() {
+int see_more(FILE * cmd) {
     int c;
     printf("\033[7m more?\033[m");
-    while ((c = getchar()) != EOF) {
+    while ((c = getc(cmd)) != EOF) {
         if (c == 'q')
             return 0;
         if (c == ' ')
