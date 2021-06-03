e188d45ef8f81f9e354591224f4b02cfccceddb7
Alex-Lin-Fei <2055017970@qq.com>
2021-06-03 11:48:48 +0800

    file IO

---
+++ APUE/chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter3.dir/main.c.o"
+  "chapter3"
+  "chapter3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
