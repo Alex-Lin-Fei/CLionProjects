a9532f9e76ffef99fec2e4340d500d53030efe48
Alex-Lin-Fei <2055017970@qq.com>
2021-06-06 10:36:15 +0800

    using structures and pointers

---
+++ PointsOnC/chapter12/cmake-build-debug/CMakeFiles/chapter12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter12.dir/main.c.o"
+  "chapter12"
+  "chapter12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
