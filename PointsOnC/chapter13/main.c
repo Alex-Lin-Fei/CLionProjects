7ec457221549ff2325851474aabb5c41c7f619e2
Alex-Lin-Fei <2055017970@qq.com>
2021-06-08 21:20:16 +0800

    add note and exercise 1

--- PointsOnC/chapter13/main.c
+++ PointsOnC/chapter13/main.c
@@ -1,6 +1,81 @@
 #include <stdio.h>
+#include <ctype.h>
+
+/*
+ * program 4
+ * */
+void binary_to_ascii(unsigned int value) {
+    unsigned int quotient;
+
+    quotient = value/10;
+    if (quotient)
+        binary_to_ascii(quotient);
+    putchar(value % 10 + '0');
+}
+
+
+/*
+ * exercise 1
+ * */
+
+/*
+ * define a function to judge whether a ch can print
+ * */
+int is_not_print(int ch) {
+    return !isprint(ch);
+}
+
+
+/*
+ * 区别每种类型的分类函数的跳转表
+ * */
+static int(*test_func[])(int) = {
+        iscntrl,
+        isspace,
+        isdigit,
+        islower,
+        isupper,
+        ispunct,
+        is_not_print
+};
+
+#define N_CATECOGIES (sizeof(test_func) / sizeof(test_func[0]))
+
+char *label[] = {
+        "control",
+        "space",
+        "digit",
+        "islower",
+        "isupper",
+        "ispunct",
+        "is_not_print"
+};
+
+int count[N_CATECOGIES];
+int total;
+
+void printChar() {
+    int ch;
+    int category;
+
+    while ((ch = getchar()) != EOF) {
+        total++;
+
+        for (category = 0; category < N_CATECOGIES; category++) {
+            if (test_func[category](ch))
+                count[category]++;
+        }
+    }
+    if (total == 0)
+        printf("No characters in the print\n");
+    else {
+        for (category = 0; category < N_CATECOGIES; category++) {
+            printf("%3.0f%% %s characters\n", count[category] * 100.0 / total, label[category]);
+        }
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
