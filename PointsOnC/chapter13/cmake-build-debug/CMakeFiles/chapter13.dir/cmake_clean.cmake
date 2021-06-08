c83fd81806cbadb5bb70ebc27122f68d06c8576d
Alex-Lin-Fei <2055017970@qq.com>
2021-06-08 20:27:28 +0800

    advanced pointers topics

---
+++ PointsOnC/chapter13/cmake-build-debug/CMakeFiles/chapter13.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter13.dir/main.c.o"
+  "chapter13"
+  "chapter13.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
